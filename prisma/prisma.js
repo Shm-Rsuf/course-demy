8e1434bcfd7171bd4889247264baac8ccbd5b187
shm_usuf <shmusuf@gmail.com>
2023-05-04 13:42:32 +0600

    prisma client and prisma model added

---
+++ prisma/prisma.js
@@ -0,0 +1,14 @@
+import { PrismaClient } from "@prisma/client";
+
+let prisma;
+
+if (process.env.NODE_ENV === "production") {
+  prisma = new PrismaClient();
+} else {
+  if (!global.prisma) {
+    global.prisma = new PrismaClient();
+  }
+  global.prisma = prisma;
+}
+
+export default prisma;
