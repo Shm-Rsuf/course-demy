37f0683e3f378b2dfa0cdb29007f12d5584c02aa
shm_usuf <shmusuf@gmail.com>
2023-05-15 21:30:36 +0600

    responsive contact added

--- pages/contact/index.jsx
+++ pages/contact/index.jsx
@@ -1,14 +1,10 @@
 const ContactPage = () => {
   return (
-    <div className="min-h-screen my-10 flex gap-10">
-      <div className="w-3/5 flex flex-col items-center gap-5">
+    <div className="min-h-screen my-10 p-3 lg:p-0 flex flex-col lg:flex-row gap-10">
+      <div className="flex flex-col items-center gap-5 lg:w-3/5">
         <h2 className="text-3xl text-teal-400 font-semibold">Contact Me</h2>
-        <form
-          action="https://formspree.io/f/mbjewagj"
-          method="POST"
-          className="w-full flex flex-col justify-center items-center gap-2"
-        >
-          <div className="form-control w-4/5">
+        <form className="w-full lg:w-4/5 flex flex-col gap-5">
+          <div className="form-control w-full">
             <label
               className="text-lg hover:text-teal-300 duration-300 cursor-pointer after:content-['*'] after:ml-0.5 after:text-red-500 font-medium"
               htmlFor="name"
@@ -16,7 +12,7 @@ const ContactPage = () => {
               Name
             </label>
             <input
-              className="w-full py-2 px-2 bg-transparent rounded border focus:outline-none focus:border-teal-300"
+              className="w-full py-2 px-2 bg-transparent rounded border outline-none focus:border-teal-300"
               type="text"
               id="name"
               name="name"
@@ -25,7 +21,7 @@ const ContactPage = () => {
             />
           </div>
 
-          <div className="form-control w-4/5">
+          <div className="form-control w-full">
             <label
               className="text-lg hover:text-teal-300 duration-300 cursor-pointer after:content-['*'] after:ml-0.5 after:text-red-500 font-medium"
               htmlFor="email"
@@ -33,7 +29,7 @@ const ContactPage = () => {
               Email
             </label>
             <input
-              className="w-full py-2 px-2 bg-transparent rounded border focus:outline-none focus:border-teal-300"
+              className="w-full py-2 px-2 bg-transparent rounded border outline-none focus:border-teal-300"
               type="email"
               id="email"
               name="email"
@@ -42,9 +38,9 @@ const ContactPage = () => {
             />
           </div>
 
-          <div className="form-control w-4/5 flex flex-col">
+          <div className="form-control w-full">
             <label
-              className="text-lg hover:text-teal-300 duration-300 cursor-pointer after:content-['*'] after:ml-0.5 after:text-red-500 font-medium"
+              className="text-lg hover:text-teal-300 duration-300 cursor-pointer after:content-['*'] after:ml-0.5 after:text-red-500 font-medium inline"
               htmlFor="message"
             >
               Message
@@ -60,32 +56,31 @@ const ContactPage = () => {
           </div>
           <button
             type="submit"
-            className="border py-2 mt-5 px-6 w-[15rem] uppercase tracking-wider bg-teal-400 text-teal-50 hover:bg-teal-500 duration-300"
+            className="border border-teal-500 rounded py-2 w-full lg:w-2/5 uppercase tracking-wider bg-teal-300 text-teal-50 hover:bg-teal-500 duration-300"
           >
-            submit
+            send message
           </button>
         </form>
       </div>
 
-      <div className="form-right p-5 w-2/5 flex flex-col gap-5">
-        <div className="form-right-top">
-          <address className="flex flex-col gap-3">
-            <div className="present-add">
-              <h4 className="text-2xl font-bold">Present Adress:</h4>
-              <p>Islamic University</p>
-              <p>Kushtia, Bangladesh</p>
-            </div>
-            <div className="permanent-add">
-              <h4 className="text-2xl font-bold">Present Adress:</h4>
-              <p>Bogura, Rajshahi, Bangladesh</p>
-            </div>
-          </address>
-        </div>
+      <div className="form-right lg:w-2/5 flex flex-col lg:justify-evenly gap-5">
+        <address className="flex flex-col justify-between gap-10">
+          <div className="present-add">
+            <h4 className="text-2xl font-bold">Present Adress:</h4>
+            <p>Islamic University</p>
+            <p>Kushtia, Bangladesh</p>
+          </div>
+          <div className="permanent-add">
+            <h4 className="text-2xl font-bold">Present Adress:</h4>
+            <p>Bogura, Rajshahi, Bangladesh</p>
+          </div>
+        </address>
+
         <div className="form-right-down">
           <iframe
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d115860.91759624307!2d89.2878040411095!3d24.841514597346322!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39fc54e7e81df441%3A0x27133ed921fe73f4!2sBogura!5e0!3m2!1sen!2sbd!4v1684148065337!5m2!1sen!2sbd"
             width="400"
-            height="320"
+            height="300"
             style={{ border: 0 }}
             allowfullscreen=""
             loading="lazy"
