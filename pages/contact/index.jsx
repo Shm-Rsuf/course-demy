fcad50834f6d2771d6af754c699ae3b175d07f2c
shm_usuf <shmusuf@gmail.com>
2023-05-15 17:39:04 +0600

    contact page added

--- pages/contact/index.jsx
+++ pages/contact/index.jsx
@@ -3,7 +3,11 @@ const ContactPage = () => {
     <div className="min-h-screen my-10 flex gap-10">
       <div className="w-3/5 flex flex-col items-center gap-5">
         <h2 className="text-3xl text-teal-400 font-semibold">Contact Me</h2>
-        <form className="w-full flex flex-col justify-center items-center gap-2">
+        <form
+          action="https://formspree.io/f/mbjewagj"
+          method="POST"
+          className="w-full flex flex-col justify-center items-center gap-2"
+        >
           <div className="form-control w-4/5">
             <label
               className="text-lg hover:text-teal-300 duration-300 cursor-pointer after:content-['*'] after:ml-0.5 after:text-red-500 font-medium"
@@ -63,9 +67,32 @@ const ContactPage = () => {
         </form>
       </div>
 
-      <div className="form-right p-5 w-2/5 flex flex-col items-center gap-5">
-        <div className="form-right-top">right top</div>
-        <div className="form-right-down">right down</div>
+      <div className="form-right p-5 w-2/5 flex flex-col gap-5">
+        <div className="form-right-top">
+          <address className="flex flex-col gap-3">
+            <div className="present-add">
+              <h4 className="text-2xl font-bold">Present Adress:</h4>
+              <p>Islamic University</p>
+              <p>Kushtia, Bangladesh</p>
+            </div>
+            <div className="permanent-add">
+              <h4 className="text-2xl font-bold">Present Adress:</h4>
+              <p>Bogura, Rajshahi, Bangladesh</p>
+            </div>
+          </address>
+        </div>
+        <div className="form-right-down">
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d115860.91759624307!2d89.2878040411095!3d24.841514597346322!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39fc54e7e81df441%3A0x27133ed921fe73f4!2sBogura!5e0!3m2!1sen!2sbd!4v1684148065337!5m2!1sen!2sbd"
+            width="400"
+            height="320"
+            style={{ border: 0 }}
+            allowfullscreen=""
+            loading="lazy"
+            referrerpolicy="no-referrer-when-downgrade"
+            className="w-full"
+          ></iframe>
+        </div>
       </div>
     </div>
   );
