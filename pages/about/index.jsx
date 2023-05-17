60fc34ae0f6e48e1e0fb471e21c841eda2109a99
shm_usuf <shmusuf@gmail.com>
2023-05-17 21:59:43 +0600

    about me page added

--- pages/about/index.jsx
+++ pages/about/index.jsx
@@ -1,9 +1,75 @@
+import Button from "@/components/Button";
+import Image from "next/image";
+import Link from "next/link";
+import { FaFacebook, FaLinkedin, FaGithub, FaTwitter } from "react-icons/fa";
+
 const AboutPage = () => {
   return (
-    <div className="wrapper my-10 min-h-screen">
+    <div className="wrapper my-10 min-h-screen lg:w-3/4">
       <h2 className="text-4xl text-center text-teal-400 font-semibold capitalize">
         about me
       </h2>
+      <div className="about-me-container my-10 flex flex-col lg:flex-row lg:gap-20 gap-10">
+        <div className="about-me-left flex flex-col items-center gap-5 lg:w-2/5 lg:h-[80vh] bg-gray-200 p-2">
+          <div className="about-img">
+            <Image
+              src="https://images.pexels.com/photos/5926389/pexels-photo-5926389.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+              alt="developer-image"
+              width={320}
+              height={320}
+              className="w-28 h-28 rounded-full"
+            />
+          </div>
+          <h2 className="text-4xl text-teal-400 font-bold">Hello, I'm Usuf</h2>
+          <p className="w-8/12 text-justify">
+            I'm a front-end developer, bridging design and technology to create
+            engaging digital experiences. Always exploring new technologies.
+          </p>
+
+          <div className="socials-links flex justify-center items-center gap-3 mt-10">
+            <Link href="/">
+              <span>
+                <FaFacebook />
+              </span>
+            </Link>
+            <Link href="/">
+              <span>
+                <FaLinkedin />
+              </span>
+            </Link>
+            <Link href="/">
+              <span>
+                <FaGithub />
+              </span>
+            </Link>
+            <Link href="/">
+              <span>
+                <FaTwitter />
+              </span>
+            </Link>
+          </div>
+        </div>
+        <div className="about-me-right flex justify-evenly flex-col gap-5 lg:w-2/4">
+          <p className="text-lg text-justify">
+            I am a proficient Front-End-Developer with expertise in creating
+            dynamic and user-friendly websites and applications. I have a great
+            experience with JavaScript, React, Next, and creative design with
+            Tailwind. I am able to create efficient, clean code that delivers
+            unique outcomes. In addition to my technical expertise, I am also a
+            creative mind with a keen eye for design, enabling me to create
+            outstanding and visually appealing web applications. With my passion
+            for excellence and dedication to achieving success, I am confident
+            that I would be an asset to any team.
+          </p>
+
+          <Button
+            href="/"
+            placeholder="Check Details"
+            color="primary"
+            size="default"
+          />
+        </div>
+      </div>
     </div>
   );
 };
