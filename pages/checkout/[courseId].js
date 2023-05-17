e418baf48d77851e2fa54340ca6fbbc0ea11347e
shm_usuf <shmusuf@gmail.com>
2023-05-18 00:44:02 +0600

    checkout serverside and session added

--- pages/checkout/[courseId].js
+++ pages/checkout/[courseId].js
@@ -1,5 +1,44 @@
-const checkoutPage = () => {
+import { getCourse } from "@/prisma/courses";
+import { useSession } from "next-auth/react";
+import { useEffect, useState } from "react";
+
+const checkoutPage = ({ course }) => {
+  const { data: session } = useSession();
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    mobile: "",
+    address: "",
+    courseTitle: course.title,
+    price: course.price,
+  });
+
+  useEffect(() => {
+    if (session) {
+      setFormData((prev) => ({
+        ...prev,
+        name: session.user.email,
+      }));
+    }
+  }, [session]);
+
   return <div>checkoutPage</div>;
 };
 
 export default checkoutPage;
+
+export const getServerSideProps = async ({ query }) => {
+  const course = await getCourse(query.courseId);
+
+  const updatedCourse = {
+    ...course,
+    createdAt: course.createdAt.toString(),
+    updatedAt: course.updatedAt.toString(),
+  };
+
+  return {
+    props: {
+      course: updatedCourse,
+    },
+  };
+};
