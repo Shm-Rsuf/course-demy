8d8f84ceb570e89a3a675aac3c05e027317ed090
shm_usuf <shmusuf@gmail.com>
2023-05-18 01:05:27 +0600

    checkout form added

--- pages/checkout/[courseId].js
+++ pages/checkout/[courseId].js
@@ -1,3 +1,4 @@
+import SectionHeader from "@/components/SectionHeader";
 import { getCourse } from "@/prisma/courses";
 import { useSession } from "next-auth/react";
 import { useEffect, useState } from "react";
@@ -22,7 +23,87 @@ const checkoutPage = ({ course }) => {
     }
   }, [session]);
 
-  return <div>checkoutPage</div>;
+  return (
+    <div className="wrapper py-10 min-h-screen">
+      <SectionHeader
+        span="checkout"
+        h2="Please provide your details"
+        p="Fill out this from to continue checkout"
+      />
+
+      <form>
+        <div className="form-control">
+          <label htmlFor="name">Name</label>
+          <input
+            type="text"
+            id="name"
+            placeholder="Enter your name"
+            value={formData.name}
+            readOnly
+          />
+        </div>
+
+        <div className="form-control">
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            id="email"
+            placeholder="hello@dev.com"
+            value={formData.email}
+            readOnly
+          />
+        </div>
+
+        <div className="form-control">
+          <label htmlFor="mobile">Phone Number</label>
+          <input
+            type="tel"
+            id="mobile"
+            placeholder="+88017xxxxxxxx"
+            value={formData.mobile}
+            onChange={(e) =>
+              setFormData({ ...formData, mobile: e.target.value })
+            }
+          />
+        </div>
+
+        <div className="form-control">
+          <label htmlFor="address">Address</label>
+          <input
+            type="text"
+            id="address"
+            placeholder="House no"
+            value={formData.address}
+            onChange={(e) =>
+              setFormData({ ...formData, address: e.target.value })
+            }
+          />
+        </div>
+
+        <div className="form-control">
+          <label htmlFor="courseTitle">Course title</label>
+          <input
+            type="text"
+            id="courseTitle"
+            placeholder="Advance javascript"
+            value={formData.courseTitle}
+            readOnly
+          />
+        </div>
+
+        <div className="form-control">
+          <label htmlFor="price">Price in (USD)</label>
+          <input
+            type="number"
+            id="price"
+            placeholder="$100"
+            value={formData.price}
+            readOnly
+          />
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default checkoutPage;
