28d86825fa910b45e31b12194573f854e8944d82
shm_usuf <shmusuf@gmail.com>
2023-05-20 20:22:53 +0600

    checkout form added

--- pages/checkout/[courseId].js
+++ pages/checkout/[courseId].js
@@ -18,7 +18,8 @@ const checkoutPage = ({ course }) => {
     if (session) {
       setFormData((prev) => ({
         ...prev,
-        name: session.user.email,
+        name: session.user.name,
+        email: session.user.email,
       }));
     }
   }, [session]);
@@ -31,95 +32,101 @@ const checkoutPage = ({ course }) => {
         p="Fill out this from to continue checkout"
       />
 
-      <form className="flex flex-col gap-5">
-        <div className="form-control flex flex-col gap-2">
-          <label htmlFor="name" className="cursor-pointer">
-            Name
-          </label>
-          <input
-            className="outline-none border py-2 px-2 focus:border-teal-300 rounded"
-            type="text"
-            id="name"
-            placeholder="Enter your name"
-            value={formData.name}
-            readOnly
-          />
-        </div>
+      <div className="flex justify-center">
+        <form className="flex flex-col gap-5 mt-10 w-full lg:w-1/2">
+          <div className="form-control flex flex-col gap-2">
+            <label htmlFor="name" className="cursor-pointer">
+              Name
+            </label>
+            <input
+              className="outline-none border py-2 px-2 focus:border-teal-300 rounded"
+              type="text"
+              id="name"
+              placeholder="Enter your name"
+              value={formData.name}
+              readOnly
+            />
+          </div>
 
-        <div className="form-control flex flex-col gap-2">
-          <label htmlFor="email" className="cursor-pointer">
-            Email
-          </label>
-          <input
-            className="outline-none border py-2 px-2 focus:border-teal-300 rounded"
-            type="email"
-            id="email"
-            placeholder="hello@dev.com"
-            value={formData.email}
-            readOnly
-          />
-        </div>
+          <div className="form-control flex flex-col gap-2">
+            <label htmlFor="email" className="cursor-pointer">
+              Email
+            </label>
+            <input
+              className="outline-none border py-2 px-2 focus:border-teal-300 rounded"
+              type="email"
+              id="email"
+              placeholder="hello@dev.com"
+              value={formData.email}
+              readOnly
+            />
+          </div>
 
-        <div className="form-control flex flex-col gap-2">
-          <label htmlFor="mobile" className="cursor-pointer">
-            Phone Number
-          </label>
-          <input
-            className="outline-none border py-2 px-2 focus:border-teal-300 rounded"
-            type="tel"
-            id="mobile"
-            placeholder="+88017xxxxxxxx"
-            value={formData.mobile}
-            onChange={(e) =>
-              setFormData({ ...formData, mobile: e.target.value })
-            }
-          />
-        </div>
+          <div className="form-control flex flex-col gap-2">
+            <label htmlFor="mobile" className="cursor-pointer">
+              Phone Number
+            </label>
+            <input
+              className="outline-none border py-2 px-2 focus:border-teal-300 rounded"
+              type="tel"
+              id="mobile"
+              placeholder="+88017xxxxxxxx"
+              value={formData.mobile}
+              onChange={(e) =>
+                setFormData({ ...formData, mobile: e.target.value })
+              }
+            />
+          </div>
 
-        <div className="form-control flex flex-col gap-2">
-          <label htmlFor="address" className="cursor-pointer">
-            Address
-          </label>
-          <input
-            className="outline-none border py-2 px-2 focus:border-teal-300 rounded"
-            type="text"
-            id="address"
-            placeholder="House no"
-            value={formData.address}
-            onChange={(e) =>
-              setFormData({ ...formData, address: e.target.value })
-            }
-          />
-        </div>
+          <div className="form-control flex flex-col gap-2">
+            <label htmlFor="address" className="cursor-pointer">
+              Address
+            </label>
+            <input
+              className="outline-none border py-2 px-2 focus:border-teal-300 rounded"
+              type="text"
+              id="address"
+              placeholder="House no"
+              value={formData.address}
+              onChange={(e) =>
+                setFormData({ ...formData, address: e.target.value })
+              }
+            />
+          </div>
 
-        <div className="form-control flex flex-col gap-2">
-          <label htmlFor="courseTitle" className="cursor-pointer">
-            Course title
-          </label>
-          <input
-            className="outline-none border py-2 px-2 focus:border-teal-300 rounded"
-            type="text"
-            id="courseTitle"
-            placeholder="Advance javascript"
-            value={formData.courseTitle}
-            readOnly
-          />
-        </div>
+          <div className="form-control flex flex-col gap-2">
+            <label htmlFor="courseTitle" className="cursor-pointer">
+              Course title
+            </label>
+            <input
+              className="outline-none border py-2 px-2 focus:border-teal-300 rounded"
+              type="text"
+              id="courseTitle"
+              placeholder="Advance javascript"
+              value={formData.courseTitle}
+              readOnly
+            />
+          </div>
 
-        <div className="form-control flex flex-col gap-2">
-          <label htmlFor="price" className="cursor-pointer">
-            Price in (USD)
-          </label>
-          <input
-            className="outline-none border py-2 px-2 focus:border-teal-300 rounded"
-            type="number"
-            id="price"
-            placeholder="$100"
-            value={formData.price}
-            readOnly
-          />
-        </div>
-      </form>
+          <div className="form-control flex flex-col gap-2">
+            <label htmlFor="price" className="cursor-pointer">
+              Price in (USD)
+            </label>
+            <input
+              className="outline-none border py-2 px-2 focus:border-teal-300 rounded"
+              type="number"
+              id="price"
+              placeholder="$100"
+              value={formData.price}
+              readOnly
+            />
+          </div>
+
+          <button className="uppercase tracking-wider bg-teal-400 text-teal-200 hover:bg-teal-500 hover:text-teal-50 duration-300 rounded py-2 w-full">
+            proceed to checkout
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
