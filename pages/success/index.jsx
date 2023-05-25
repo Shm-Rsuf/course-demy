37954aaee147e0c13b046b8617f01c0f0b95f44f
shm_usuf <shmusuf@gmail.com>
2023-05-25 20:01:23 +0600

    success page added

--- pages/success/index.jsx
+++ pages/success/index.jsx
@@ -1,5 +1,24 @@
+import Button from "@/components/Button";
+import { AiOutlineCheckCircle } from "react-icons/ai";
+
 const SuccessPage = () => {
-  return <div className="wrapper min-h-screen py-10">SuccessPage</div>;
+  return (
+    <div className="wrapper min-h-screen py-10">
+      <div className="flex flex-col items-center gap-5">
+        <h2 className="text-2xl flex items-center gap-2">
+          <span className="text-emerald-500">
+            <AiOutlineCheckCircle />
+          </span>
+          You've enrolled successfully
+        </h2>
+        <Button
+          href="/orders"
+          placeholder="Go to your orders"
+          color="secondary"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default SuccessPage;
