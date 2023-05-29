a56978222f1e1aa66f4fdc0033c7aeaa58100da3
shm_usuf <shmusuf@gmail.com>
2023-05-29 15:37:59 +0600

    framer motions added

--- pages/users/dashbord/courses/[courseId].js
+++ pages/users/dashbord/courses/[courseId].js
@@ -1,5 +1,27 @@
-const VideosPage = () => {
-  return <div>VideosPage</div>;
+import { getCourse } from "@/prisma/courses";
+
+const VideosPage = ({ course }) => {
+  console.log(course);
+  return (
+    <div className="wrapper py-10 min-h-screen">
+      <h2>{course.title}</h2>
+    </div>
+  );
 };
 
 export default VideosPage;
+export const getServerSideProps = async ({ query }) => {
+  const course = await getCourse(query.courseId);
+
+  const updatedCourse = {
+    ...course,
+    createdAt: course.createdAt.toString(),
+    updatedAt: course.updatedAt.toString(),
+  };
+
+  return {
+    props: {
+      course: updatedCourse,
+    },
+  };
+};
