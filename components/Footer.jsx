9eab30f606866cdc679fa77396a4b6f248369c78
shm_usuf <shmusuf@gmail.com>
2023-06-20 22:28:36 +0600

    coursedemy done

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,11 +1,13 @@
 const Footer = () => {
   return (
-    <footer className="flex-center bg-gray-900 text-gray-50 py-5">
-      <p>
-        &copy; {new Date().getFullYear()}{" "}
-        <span className="text-rose-500 capitalize">c</span>ourse
-        <span className="text-teal-400">Demy</span>. All rights reserved
-      </p>
+    <footer>
+      <div className="flex-center bg-gray-900 text-gray-50 py-5">
+        <p>
+          &copy; {new Date().getFullYear()}{" "}
+          <span className="text-rose-500 capitalize">c</span>ourse
+          <span className="text-teal-400">Demy</span>. All rights reserved
+        </p>
+      </div>
     </footer>
   );
 };
